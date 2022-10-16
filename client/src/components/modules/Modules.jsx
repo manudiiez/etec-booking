6d5e218a2db4469292a0881d06237433b3bfb3bd
manudiiez <manudiiez123@gmail.com>
2022-10-16 19:28:58 -0300

    eventos y calendario

--- client/src/components/modules/Modules.jsx
+++ client/src/components/modules/Modules.jsx
@@ -1,11 +1,29 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+const Events = [
+    {
+      'title': 'Programacion 1',
+      'allDay': true,
+      'start': new Date(2022, 9, 14), // 10.00 AM
+      'end': new Date(2022, 9, 14), // 10.00 AM
+    },
+]
+
+const localizer = momentLocalizer(moment);
+
 
 const Modules = ({ labid, dates }) => {
   const [selectedModules, setSelectedModules] = useState([]);
+  const [myEvents, setMyEvents] = useState();
+
 
   const { user } = useContext(AuthContext)
 
@@ -30,10 +48,8 @@ const Modules = ({ labid, dates }) => {
   };
 
   const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
-  console.log(alldates);
 
   const isAvalible = (dateNumber) => {
-
     const isFound = dateNumber.unavailableDates.some((date) => 
         alldates.includes(new Date(date.date).getTime())
     );
@@ -52,11 +68,12 @@ const Modules = ({ labid, dates }) => {
   };
 
   const handleClick = async () => {
+    console.log('reservar')
     try {
       await Promise.all(
         selectedModules.map((module) => {
           alldates.map((selectedDate) => {
-            console.log(module, selectedDate)
+            const start = new Date(selectedDate).setHours()
             const res = axios.put(`/module/availability/${module}`, {
                 subjectName: 'programacion',
                 teacherName: user.fullname,
@@ -70,6 +87,36 @@ const Modules = ({ labid, dates }) => {
     } catch (err) {}
   };
 
+  const getEventsDate = async () => {
+    const events = []
+    data.map((module) => {
+        module.unavailableDates.map((event) => {
+            const startDate = new Date(event.date)
+            const endDate = new Date(event.date)
+            startDate.setHours(module.startHour, module.startTime)
+            endDate.setHours(module.endHour, module.endTime)
+            const newEvent = {
+                title: event.subjectName,
+                allDay: false,
+                start: startDate,
+                end: endDate,
+                id: event._id
+            }
+            events.push(newEvent)
+        })
+    })
+    setMyEvents(events)
+    console.log(events)
+  }
+
+
+
+  useEffect(() => {
+    if(data){
+        getEventsDate()
+    }
+  },[data])
+
   return (
     <div>
       <p>Modules</p>
@@ -78,14 +125,26 @@ const Modules = ({ labid, dates }) => {
           <span className="visually-hidden">Loading...</span>
         </div>
       ) : (
-        <ul className="list-group">
-          {data.map((item) => (
-            <li key={item._id} className="list-group-item">
-              {item.name}
-              <input type="checkbox" value={item._id} disabled={!isAvalible(item)} onChange={handleSelect} />
-            </li>
-          ))}
-        </ul>
+        <>
+            <Calendar
+                localizer={localizer}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+                events={myEvents}
+                selectable={true}
+                defaultView='week'
+                onSelectEvent={(event) => console.log(event)}
+            />
+            <ul className="list-group">
+            {data.map((item) => (
+                <li key={item._id} className="list-group-item">
+                {item.name}
+                <input type="checkbox" value={item._id} disabled={!isAvalible(item)} onChange={handleSelect} />
+                </li>
+            ))}
+            </ul>
+        </>
       )}
       <button className="btn btn-primary mt-5" onClick={handleClick}>Reservar</button>
     </div>
